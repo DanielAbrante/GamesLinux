71815235e3c5d6762b660baeb35c94ab790a58f3
Daniel Abrante <danielabrante@protonmail.com>
2022-02-04 16:08:01 -0300

    Changed method download games

--- gameslinux.sh
+++ gameslinux.sh
@@ -16,15 +16,77 @@ about() {
 	echo ""
 }
 
+get_games_list() {
+	NUMBER_LINES=`wc -l titles.txt | grep -o "[0-9]*"`
+
+	for ((i = 1; i <= $NUMBER_LINES; i++))
+	do
+		TITLES[$i]=`sed ""$i'!d'"" ./titles.txt`
+	done
+
+	for ((i = 1; i <= $NUMBER_LINES; i++))
+	do
+		echo "ID=$i -" ${TITLES[$i]}
+	done
+}
+
+get_game_id() {
+	echo ""
+	echo '===================================='
+	echo ' Choose the ID to download the game '
+	echo ""
+	printf 'ID: '
+	read -r GAME_ID
+}
+
+APPLICATION="1"
+
 about
 
-printf 'Type a game: '
-read -r GAME
-echo ""
+while [ "$APPLICATION" = "1" ]
+do
+	echo "[a] - All games list"
+	echo "[s] - Search game"
+	echo "[e] - Exit"
+	printf '>> '
+	read -r OPTION_MENU
+	
+	if [ "$OPTION_MENU" = "a" ] 
+	then
+		curl https://yewtu.be/channel/UCrE7Vzc6G971XnrDyo53mGA | grep -A 1 -io '<p dir="auto">[- a-zA-Z0-9.á-źçã,!]*' | sed 's/<p dir="auto">//g' | sed 's/--//g' | grep -v '^[[:space:]]*$' > titles.txt
+		echo ""
+		get_games_list
+		get_game_id
+		APPLICATION="0"
+	elif [ "$OPTION_MENU" = "s" ] 
+	then
+		echo ""
+		printf 'Type a name: '
+		read -r GAME
+		echo ""
+		echo ""
+		curl https://yewtu.be/channel/UCrE7Vzc6G971XnrDyo53mGA | grep -A 1 -io "$GAME[- a-zA-Z0-9.á-źçã,]*" | sed 's/<p dir="auto">//g' | sed 's/--//g' | grep -v '^[[:space:]]*$' > titles.txt
+		get_games_list
+		get_game_id
+		APPLICATION="0"
+	elif [ "$OPTION_MENU" = "e" ]
+	then
+		exit 1
+	else 
+		`zenity --error --text='Invalid Option!' --title='Erro'` 
+		clear
+	fi
+done
+
+printf 'Choose the filename, (No need extension!): '
+read -r FILENAME
 
 URL_INVIDIOUS_INSTANCE='https://yewtu.be'
 
-HTML_GAME_TITLE=`curl https://yewtu.be/channel/UCrE7Vzc6G971XnrDyo53mGA | grep -B 11 -i $GAME`
+HTML_GAME_TITLE=`curl https://yewtu.be/channel/UCrE7Vzc6G971XnrDyo53mGA | grep -B 11 -i "${TITLES[GAME_ID]}"`
+
+rm -f ./titles.txt
+
 URL_GAME_TITLE=`echo $HTML_GAME_TITLE | grep -oi '/watch?v=[a-zA-Z0-9]*'`
 HTML_VIDEO=$URL_INVIDIOUS_INSTANCE$URL_GAME_TITLE
 FILEID=`curl $HTML_VIDEO | grep -m 1 -i 'drive' | grep -io '1[_a-zA-Z0-9-]*'`
@@ -34,7 +96,7 @@ mkdir -p GamesLinux
 cd GamesLinux
 
 curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${FILEID}" > /dev/null
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${FILEID}" -o ${GAME}.AppImage
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${FILEID}" -o ${FILENAME}.AppImage
 
-chmod +x $GAME.AppImage
+chmod +x $FILENAME.AppImage
 rm -f cookie
